09f10f2d58037ed981377585a66a8f4212b273c0
walframich <segeda.v2007@gmail.com>
2025-12-18 16:58:13 -0500

    comments added

--- portfolio_tracker.py
+++ portfolio_tracker.py
@@ -9,8 +9,8 @@ from pathlib import Path
 #current problem: updating the most recent price
 
 def main():
-    FILENAME = Path("stock_tracker.xlsx")
-    if FILENAME.is_file():
+    FILENAME = Path("investment-tracker/stock_tracker.xlsx")
+    if FILENAME.exists():
         print("entering the excel file...")
     else:
         create_excel()
@@ -20,7 +20,7 @@ def main():
         ticker, start_date, end_date, shares_number = add_ticker()
         prices = load_prices(ticker, start_date, end_date)
         purchase_price = prices.loc[start_date].iloc[0]
-        current_price = prices.loc["2025-12-11"].iloc[0] #simulating the last date
+        current_price = prices.loc[end_date].iloc[0] #simulating the last date
         if ticker_owned(FILENAME, ticker):
             portfolio = pd.read_excel(FILENAME, index_col = "Ticker")
             start_price, num_owned = weighted_average(FILENAME, ticker, shares_number, purchase_price) 
@@ -28,7 +28,6 @@ def main():
             portfolio.at[ticker, "Purchase price"] = start_price    
             portfolio.at[ticker, "% change"] = percent_change(start_price, current_price)    
             # start price is the weighted average of prices and num_owned is number u owned + number bought
-            ## data = format_for_excel(ticker, num_owned, start_price, current_price, percent_change(purchase_price, current_price))
             print(portfolio)
             update_to_excel(portfolio, FILENAME)
 
@@ -37,7 +36,7 @@ def main():
             export_to_excel(data, FILENAME)
 
     elif action == "sell":
-        ticker, sale_date, shares_sold = delete_ticker()
+        ticker, shares_sold = delete_ticker()
         if ticker_owned(FILENAME, ticker):
             portfolio = pd.read_excel(FILENAME, index_col= "Ticker")
             num_owned = remove_shares(FILENAME, ticker, shares_sold)
@@ -57,9 +56,9 @@ def main():
 ## CALCULATIONS
 def prepare_summary(FILENAME: str) -> tuple[int, float, int, float, float]:
     """
-    claculates the summary statistics of the portfolio
+    calculates the summary statistics of the portfolio
     
-    :return: Description
+    :return: returns the number of holdings owned, total sum invested, total shares owned, average price per share, overall percent growth
     :rtype: tuple[int, float, int, float, float]
     """
     portfolio = pd.read_excel(FILENAME)
@@ -72,6 +71,18 @@ def prepare_summary(FILENAME: str) -> tuple[int, float, int, float, float]:
     return num_holdings, total_sum, total_shares, average_price, overall_percent
 
 def remove_shares(FILENAME: str, ticker: str, shares_sold: int) -> int:
+    """
+    removes shares when sold, only counts the shares
+    
+    :param FILENAME: const. file name to be accessed
+    :type FILENAME: str
+    :param ticker: ticker code
+    :type ticker: str
+    :param shares_sold: number of shares sold
+    :type shares_sold: int
+    :return: new number of shares owned after the sale
+    :rtype: int
+    """
     data = pd.read_excel(FILENAME, index_col = "Ticker")
     num_owned = data.loc[ticker]["Number"]
     num_owned -= shares_sold
@@ -81,11 +92,11 @@ def weighted_average(FILENAME: str, ticker: str, shares_number: int, purchase_pr
     """
     calculates the weighted average of shares bought at different prices
     
-    :param FILENAME: Description
+    :param FILENAME: const. file name to be accessed
     :type FILENAME: str
-    :param ticker: Description
+    :param ticker: ticker code
     :type ticker: str
-    :param shares_number: Description
+    :param shares_number: number of shares owned
     :type shares_number: int
     :param purchase_price: new "purchase price" - reweighted average of the stock price and the new shares number
     :type purchase_price: float
@@ -108,9 +119,9 @@ def percent_change(purchase_price:float, current_price: float) -> float:
     """
     calculated percent change from the price bought at (or weighted) till the most recent price
 
-    :param purchase_price: Description
+    :param purchase_price: price at which the share was purchased
     :type purchase_price: float
-    :param current_price: Description
+    :param current_price: current price of a share
     :type current_price: float
     :return: returns the percent change between the prices
     :rtype: float
@@ -122,9 +133,9 @@ def percent_change(purchase_price:float, current_price: float) -> float:
 ## USER CHOICES AND IMPLICATIONS
 def take_action() -> str:
     """
-    Docstring for take_action
+    choose from the actions buy/sell/update
     
-    :return: Description
+    :return: action chosen by user
     :rtype: str
     """
 
@@ -133,25 +144,22 @@ def take_action() -> str:
 
 def delete_ticker() -> tuple[str, str, int]:
     """
-    Docstring for delete_ticker
+    removes the shares of the ticker
     
-    :return: Description
+    :return: ticker from transaction and number sold
     :rtype: tuple[str, str, int]
     """
-    date = input("enter the date of sale (YYYY-MM-DD): ")
-    sale_date = pd.to_datetime(date).strftime("%Y-%m-%d")   
-
     ticker = input("Ticker name: ")
 
     shares_number = int(input("Number sold: "))
 
-    return ticker, sale_date, shares_number
+    return ticker, shares_number
 
 def add_ticker() -> tuple[str, str, str, int]:
     """
-    prompts a user to enter their transaction
+    prompts a user to enter their transaction for buying shares
     
-    :return: Description
+    :return: ticker, date when shares were bought, most recent date when market was operating, number of shares bought
     :rtype: tuple[str, str, str, int]
     """
     date = input("enter the date of purchase (YYYY-MM-DD): ")
@@ -170,9 +178,9 @@ def update_summary(summary: pd.DataFrame, FILENAME: str) -> None:
     """
     updates the summary in excel sheet
     
-    :param summary: Description
+    :param summary: formatted consolidation of changes in investments 
     :type summary: pd.DataFrame
-    :param FILENAME: Description
+    :param FILENAME: const. file name to be accessed
     :type FILENAME: str
     """
     writer = pd.ExcelWriter(FILENAME, engine = "openpyxl", mode = "a", if_sheet_exists = "replace")
@@ -185,15 +193,15 @@ def format_summary(num_holdings: int, total_sum: float, total_shares: int, avera
     """
     formats summary for excel sheet
     
-    :param num_holdings: Description
+    :param num_holdings: number of holdings owned
     :type num_holdings: int
-    :param total_sum: Description
+    :param total_sum: total capital invested
     :type total_sum: float
-    :param total_shares: Description
+    :param total_shares: total shares owned
     :type total_shares: int
-    :param average_price: Description
+    :param average_price: average price of share
     :type average_price: float
-    :param overall_percent: Description
+    :param overall_percent: overall percentage return on portfolio
     :type overall_percent: float
     """
     form = {
@@ -211,15 +219,15 @@ def format_for_excel(ticker: str, shares_number: int, purchase_price: float, end
     """
     formats an entry for excel sheet
 
-    :param ticker: Description
+    :param ticker: ticker of a company
     :type ticker: str
-    :param shares_number: Description
+    :param shares_number: number of shares owned
     :type shares_number: int
-    :param purchase_price: Description
+    :param purchase_price: purchase price of a share
     :type purchase_price: float
-    :param end_price: Description
+    :param end_price: most recent price of a share
     :type end_price: float
-    :param percent_change: Description
+    :param percent_change: percent change from the start_price till the end_price
     :type percent_change: float
     :return: a dataframe of a transaction with ticker, number of shares, buying price, recent price, and new percent change
     :rtype: DataFrame
@@ -234,9 +242,9 @@ def update_to_excel(data: pd.DataFrame, FILENAME: str) -> None:
     """
     when the ticker entered is owned the function will write down the updated info
     
-    :param data: Description
+    :param data: consolited info of a transaction
     :type data: pd.DataFrame
-    :param FILENAME: Description
+    :param FILENAME: const. file name to be accessed
     :type FILENAME: str
     """
     writer = pd.ExcelWriter(path= FILENAME, engine= "openpyxl", mode = "w")
@@ -250,7 +258,7 @@ def export_to_excel(data: pd.DataFrame, FILENAME: str) -> None:
         
     :param data: Description
     :type data: pd.DataFrame
-    :param FILENAME: Description
+    :param FILENAME: const. file name to be accessed
     :type FILENAME: str
     """
 
@@ -295,7 +303,7 @@ def update_prices(filename: str) -> pd.DataFrame:
     """
     updates the prices on request and saves them in excel
     
-    :param filename: Description
+    :param filename: const. file name to be accessed
     :type filename: str
     :return: Description
     :rtype: DataFrame
@@ -332,23 +340,25 @@ def load_prices(ticker: str,
     data.drop(["High", "Low", "Open", "Volume"], inplace= True, axis = 1)
     return data
 
-def ticker_owned(file: str, ticker:str) -> bool:
+def ticker_owned(filename: str, ticker:str) -> bool:
     """
     returns true if the ticker entered is already owned
 
-    :param file: Description
-    :type file: str
-    :param ticker: Description
+    :param filename: const. file name to be accessed
+    :type filename: str
+    :param ticker: ticker of the company
     :type ticker: str
-    :return: Description
+    :return: returns True if ticker is in excel file, and False if not
     :rtype: bool
     """
 
-    tickers = pd.read_excel(file)
+    tickers = pd.read_excel(filename)
     rows_list = tickers.values.tolist()
     for row in rows_list:
         if ticker in row:
             return True
     return False
+
+
 if __name__ == "__main__":
     main()
